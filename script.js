ed4ef19b78c3020fa3e58865a47f38e84c3f507f
mevsimdeniz <mdnzarslan@gmail.com>
2020-02-09 15:38:27 +0300

    ilk gönderimiz

---
+++ script.js
@@ -0,0 +1,127 @@
+// alert("Merhaba")
+// console.log("merhaba");
+// console.log(1234);
+// console.log(true);
+// console.log([1,2,3,4]);
+// console.warn("bir uyarı oluştu");
+// console.eror("bir hata oluştu");
+
+// tek satırlık açıklama //
+
+/*
+ 
+açıklama satırları 
+açıklama satırları 
+açıklama satırları 
+açıklama satırları 
+
+
+
+ */
+   
+
+ // Değişkenler
+
+//  var age;
+//  console.log(age);
+//  age= 27;
+//  console.log(age);
+
+//  var fullname='mevsim deniz';
+//  console.log(fullname);
+ 
+//  fullname= 'Deniz Arslan';
+//  console.log(fullname);
+
+//  // Değişken tanımlama kuralları 
+
+//  // var sayısal ifade ile başlamaz. fakat böyle tanımlanabilir;
+
+//  var yas1;
+//  var yas_2;
+//  var $yas3;
+ 
+//  // komut isimleri ile tanımlama yapılmaz 
+// // birden fazla kelime tanımlanacaksa ;
+
+// var ad='mevsim';
+// var soyad='arslan';
+ 
+// var adSoyad='mda arslan';
+// var ad_soyad= 'mevsim arslan';
+
+// console.log(ad_soyad);
+// console.log(adSoyad);
+
+// // case sensitive ( büyük küçük harf duyarlılığı)
+
+// var firstName='Mevsim';
+// var FirsName=' Deniz';
+
+// console.log(firstName);
+// console.log(FirsName);
+
+// // var , let , const 
+
+// let city= 'Berlin';
+// console.log(city);
+
+// const email='mda@gmail.com';
+// console.log(email);
+ 
+ // Ders: 3  Değişken tipleri;
+
+ //  a) Premitive types
+
+    //  // string
+    //   let firstName='nida';
+    //   console.log(typeof firstName);
+       
+    //   // number 
+    //   let age = 20;
+    //   let money = 30.4;
+    //   console.log(typeof money);
+
+    //   // boolean
+
+    //   let isAktive = false;
+    //   console.log(isAktive);
+
+    //   //null
+
+    //   let job= null;
+    //   console.log(job);
+
+    //   //underfind
+
+    //   let car;
+    //   console.log(car);
+
+    //  // b) Refercence tyeps - Objects
+ 
+    //    // arry
+ 
+    //    let names=['Ali','Ahmet','Can'];
+    //   console.log(typeof names);
+
+    //   //object
+
+    //   let address ={
+    //       city:'İzmir',
+    //       contry:'Türkiye'
+    //   }
+    //   console.log(typeof address);
+
+    //   //function
+
+    //   var calculateAge=function(){
+    //       return 0;
+    //   }
+    //   console.log(typeof calculateAge);
+
+    // 4.DERS TÜR DÖNÜŞÜMÜ 
+
+    let num1 = '5';
+    console.log(typeof num1);
+
+    //
